209388571917b9b2f22acefeba36d224cb10e85b
Till Klampaeckel <till@php.net>
2010-06-27 01:17:10 +0200

    * crunch a single file  * this will allow us to run multiple workers

---
+++ process-single.php
@@ -0,0 +1,85 @@
+#!/usr/bin/env php
+<?php
+/**
+ * @category System
+ * @package  PEAR_LogfileAnalysis
+ * @author   Till Klampaeckel <till@php.net>
+ * @license
+ * @link     http://github.com/pear/PEAR_LogfileAnalysis
+ */
+
+/**
+ * @namespace
+ */
+namespace PEAR;
+
+/**
+ * @desc Include \PEAR\LogfileAnalysis
+ */
+require_once __DIR__ . '/PEAR/LogfileAnalysis.php';
+
+LogfileAnalysis::init(); // pseudo __construct()
+
+/**
+ * @desc Argument parsing! Primarily to be able to resume the crunching.
+ *       Example: ./process-single.php /logfile lineno
+ *       The lineno argument is optional and is only used when supplied.
+ */
+$start = null;
+if (isset($argv[1])) {
+    $start = $argv[1];
+}
+if ($start === null) {
+    echo "Need a logfile to crunch.\n";
+    exit(1);
+}
+
+$lineNo = false;
+if (isset($argv[2])) {
+    $lineNo = (int) $argv[2];
+    if (empty($lineNo)) {
+        $lineNo = false;
+    } else {
+        echo "\n\tResuming at line: {$lineNo} in {$start}\n\n";
+    }
+}
+
+$logFiles = LogfileAnalysis::globr(__DIR__ . '/*');
+
+    echo "Currently crunching: {$prettyLog}\n";
+
+$handle = @fopen($log, 'r');
+if (!$handle) {
+    echo "Could not open {$prettyLog}\n";
+    exit(1);
+}
+
+$lineCount = 1;
+while (!feof($handle)) {
+    $line = fgets($handle);
+    if ($lineNo !== false) {
+        if ($lineCount < $lineNo) {
+            unset($line);
+            $lineCount++;
+            // echo "\t\tSkipping: {$lineCount} (>> {$lineNo})\n";
+            continue;
+        }
+        if ($lineCount == $lineNo) {
+            $lineNo = false; // start crunching
+        }
+    }
+
+    $doc = LogfileAnalysis::parseLine($line);
+    if ($doc === false) {
+        $lineCount++;
+        continue;
+    }
+    LogfileAnalysis::sendToCouchDB($doc, $prettyLog, $lineCount);
+
+    unset($line);
+    unset($doc);
+
+    $lineCount++;
+}
+
+echo "\t" . round((memory_get_usage(true)/1024/1024), 2) . "MB\n";
